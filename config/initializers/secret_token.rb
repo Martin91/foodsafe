23456ec522f1ff15238ea3d4dd6363c96626d70f
Martin <martin@martin-desktop.(none)>
2012-12-18 12:02:10 +0800

    Finish creating a project for food_safe_net

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodSafeNet::Application.config.secret_token = '947064e8c436579c362d0969fb7866308ab5563c208e5c1919143cce4488141b503d299cf6b3953e97762f9ee3177049936b4feffe22c50ec00b51e50b09c3b8'
