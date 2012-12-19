188b915030773a5291d88f547270a9f2110f8624
Martin <hongzeqin@gmail.com>
2012-12-19 18:15:01 +0800

    create ServersController and modify route configure file

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ FoodSafeNet::Application.routes.draw do
   match 'gov_infos' => 'gov_info#index'
   match 'public_infos' => 'public_infos#index'
   match 'reports' => 'reports#index'
+  match 'servers' => 'servers#index'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
