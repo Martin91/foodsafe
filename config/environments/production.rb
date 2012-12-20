71fc24b9af58a3c30b897d1efdfd721f5921d7ec
Martin <hongzeqin@gmail.com>
2012-12-20 23:29:30 +0800

    no

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ FoodSafeNet::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['bootstrap.js', 'global.js', 'jquery.js']
+  config.assets.precompile += ['bootstrap.js', 'global.js', 'jquery.js', 'bootstrap.css']
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
