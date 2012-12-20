db705d098e96d02fa44c0332efd5185242483644
Martin <hongzeqin@gmail.com>
2012-12-21 00:08:02 +0800

    nothing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,5 +33,5 @@ FoodSafeNet::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = false
+  config.assets.debug = true
 end
