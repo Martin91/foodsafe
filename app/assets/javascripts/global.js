8fa5583ffe74459b5250b5f7b44e339a3f630270
Martin <hongzeqin@gmail.com>
2012-12-20 16:56:28 +0800

    finish the motion of navigators in the navbar

---
+++ app/assets/javascripts/global.js
@@ -0,0 +1,18 @@
+window.onload = init_nav;
+
+function get_controller_name(){
+  path = location.href;  //获取当前页面的路径，全路径
+  path_array = path.split("//");  //分离协议
+  current_controller = path_array[1].split("/");
+  return current_controller[1];
+}
+
+function init_nav(){
+  var current_controller_name = get_controller_name();
+  var active_nav = document.getElementById("/" + current_controller_name);
+  active_nav.setAttribute("class", "active");
+}
+
+function invalid_nav(){
+  alert("本系统尚在开发中，欢迎您继续浏览本站其他精彩内容！");
+}
