d5ff69d6914310b56396a554cbaf1a87ccca340c
Martin <hongzeqin@gmail.com>
2012-12-27 20:33:30 +0800

    create fake data to laws and add a paginator for index view of laws

--- app/controllers/laws_controller.rb
+++ app/controllers/laws_controller.rb
@@ -1,7 +1,7 @@
 class LawsController < ApplicationController
   def index
     @law_types = Law.select(:law_type).uniq.map(&:law_type)
-    @laws = Law.all
+    @laws = Law.order(:created_at).page params[:page]
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+(1...1000).each do |count| 
+  law = Law.new
+  law.title = "#{count}这是一个标题"
+  law.content = "这是一段文本，用来说明发生了什么事"
+  law.law_type = "相关法律"
+  law.save
+end
