b5b34c39ec50435cb0ea39b4a49e1d4edb98159a
Martin <hongzeqin@gmail.com>
2012-12-27 21:24:50 +0800

    add fake data for laws with different types

--- app/controllers/laws_controller.rb
+++ app/controllers/laws_controller.rb
@@ -1,7 +1,13 @@
+#encoding: utf-8
+
 class LawsController < ApplicationController
   def index
     @law_types = Law.select(:law_type).uniq.map(&:law_type)
-    @laws = Law.order(:created_at).page params[:page]
+    if params[:law_type] == nil
+      @laws = Law.where("law_type = '相关法律'").page params[:page]
+    else
+      @laws = Law.where("law_type = '" + params[:law_type]+ "'").page params[:page]
+    end
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-(1...1000).each do |count| 
+count_range = 1...100
+
+count_range.each do |count| 
   law = Law.new
-  law.title = "#{count}这是一个标题"
-  law.content = "这是一段文本，用来说明发生了什么事"
+  law.title = "#{count}这是某部法律的标题"
+  law.content = "这是一段文本，说明法律内容" * 50
   law.law_type = "相关法律"
+  law.save
+
+  law = Law.new
+  law.title = "#{count}这是某部法规的标题"
+  law.content = "这是一段文本，说明法规内容" * 50
+  law.law_type = "相关法规"
+  law.save
+
+  law = Law.new
+  law.title = "#{count}这是某个部门规定的标题"
+  law.content = "这个部分说明部门规定内容" * 50
+  law.law_type = "部门规定"
+  law.save
+
+  law = Law.new
+  law.title = "#{count}这是工作文件标题"
+  law.content = "这是一段文本，说明工作文件内容" * 50
+  law.law_type = "工作文件"
   law.save
 end
