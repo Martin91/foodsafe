6d30f585c9a0b81eb5ec23e9c64576fe35f212b9
Martin <hongzeqin@gmail.com>
2012-12-29 20:45:32 +0800

    implemented the simple search function for company search and product search

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -3,12 +3,36 @@ class ServersController < ApplicationController
   end
 
   def search_company
-    @companies = Company.page(params[:page]).per(10)
+    if !params[:company_name].empty?
+      @companies = Company.where("name LIKE '%#{params[:company_name]}%'").page(params[:page]).per(10)
+    else
+      if !params[:register_address].empty?
+        @companies = Company.where("register_address LIKE '%#{params[:register_address]}%'").page(params[:page]).per(10)
+      else
+        if !params[:company_license_no].empty?
+          @companies = Company.where("license_no LIKE '%#{params[:company_license_no]}%'").page(params[:page]).per(10)
+        else
+          if !params[:license_project].empty?
+            @companies = Company.where("license_project LIKE '%#{params[:license_project]}%'").page(params[:page]).per(10)
+          else
+            @companies = Company.page(params[:page]).per(10)
+          end
+        end
+      end
+    end
     render 'companies'
   end
 
   def search_product
-    @products = Product.page(params[:page]).per(10)
+    if !params[:product_name].empty?
+      @products = Product.where("name LIKE '%#{params[:product_name]}%'").page(params[:page]).per(10)
+    else
+      if !params[:product_license_no].empty?
+        @products = Product.where("license_no LIKE '%#{params[:product_license_no]}%'").page(params[:page]).per(10)
+      else
+        @products = Product.page(params[:page]).per(10)
+      end
+    end
     render 'products'
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,24 @@
 count_range = 1...50
 count_range.each do |count|
   company = Company.new
-  company.name = "白云山&王老吉药业"
+  company.name = "白云山药业"
+  company.produce_address = "XX省XX市XX区XX县XX工业区XX食品生产加工#{count}区"
+  company.license_no = "GD·FDA健证字（2012）第0111W#{count}号"
+  company.legal_person = "洪XX"
+  company.license_project = "生产销售片剂、胶囊剂、颗粒剂、软胶囊剂"
+  company.register_address = "XX县XX工业区XX食品生产加工#{count}区"
+  company.valid_for = "20#{count}-13-32"
+  company.save
+end
+
+count_range.each do |count|
+  company = Company.new
+  company.name = "王老吉制药"
   company.produce_address = "XX省XX市XX区XX县XX工业区XX食品生产加工#{count}区"
   company.license_no = "GD·FDA健证字（2010）第0111W#{count}号"
   company.legal_person = "洪XX"
-  company.license_project = "保健食品生产销售（片剂、胶囊剂、颗粒剂、软胶囊剂）"
-  company.register_address = "XX省XX市XX区XX县XX工业区XX食品生产加工#{count}区"
+  company.license_project = "保健食品片剂、胶囊剂、颗粒剂、软胶囊剂"
+  company.register_address = "XX省XX市XX区"
   company.valid_for = "20#{count}-13-32"
   company.save
 end
@@ -16,6 +28,16 @@ count_range.each do |count|
   product = Product.new
   product.name = "仁德堂牌妙妙减肥胶囊"
   product.license_no = "卫食健字（2002）第02#{count}号"
+  product.company_name = "XX保健生物制药厂"
+  product.company_address = "XX省XX市XX区第#{count}减肥厂"
+  product.health_function = "减肥防猝死"
+  product.save
+end
+
+count_range.each do |count|
+  product = Product.new
+  product.name = "还我靓靓药"
+  product.license_no = "卫食健字（2002）第08#{count}号"
   product.company_name = "XX保健生物制药厂"
   product.company_address = "XX省XX市XX区第#{count}减肥厂"
   product.health_function = "减肥防猝死"
