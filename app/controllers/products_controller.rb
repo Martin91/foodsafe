8efdb9b64ef94323afdb947de1b1948b72df79cc
Martin <hongzeqin@gmail.com>
2013-02-21 23:31:33 +0800

    implement qrcodes and show them

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,5 +10,6 @@ class ProductsController < ApplicationController
     product = Product.find params[:id]
     product_value_string = product.name + "\n" + product.license_no + "\n" + product.company_name + "\n" + product.company_address + "\n" + product.health_function
     @qr = RQRCode::QRCode.new product_value_string, :size => 8
+    @name = product.name
   end
 end
